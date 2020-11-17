86c2f14bdf0b957b00cc26cdaf4aac0cffc925da
ChocolateyAutoUpdate <chocolatey@github.com>
2020-11-17 00:01:04 +0000

    AU: 1 updated - newrelic-mssql [skip ci] https://gist.github.com/4d4c22e3d439bfee8dd5d04bed88a371/f15ec0e506499065f274d8a91b63d2d4b58a3446

--- newrelic-mssql/tools/chocolateyinstall.ps1
+++ newrelic-mssql/tools/chocolateyinstall.ps1
@@ -7,8 +7,8 @@ $packageArgs = @{
   softwareName  = 'New Relic Infrastructure Integration, nri-mssql*'
   unzipLocation = $toolsDir
   fileType      = 'msi'
-  url           = 'https://download.newrelic.com/infrastructure_agent/windows/integrations/nri-mssql/nri-mssql-amd64.2.5.1.msi'
-  checksum      = '8a54dc0faef9601049942b82f65edbcb7c652b47c12d8c4dab46a237554b3125'
+  url           = 'https://download.newrelic.com/infrastructure_agent/windows/integrations/nri-mssql/nri-mssql-amd64.2.5.2.msi'
+  checksum      = '1f592f7cf85395fd2dfe602bc9a747f4d5a815177dfde8f3c5346c70784092f6'
   checksumType  = 'sha256'
   silentArgs    = '/qn /norestart'
   validExitCodes= @(0, 3010, 1605, 1614, 1641) # https://msdn.microsoft.com/en-us/library/aa376931(v=vs.85).aspx
