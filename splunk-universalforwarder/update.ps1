426f4d19a02fa310206a0f63d84b85431fbb49e0
David Pollock <david.pollock@judopay.com>
2021-09-24 10:28:50 +1000

    Fix splunk-universalforwarder download url

--- splunk-universalforwarder/update.ps1
+++ splunk-universalforwarder/update.ps1
@@ -26,18 +26,20 @@ function global:au_GetLatest {
 
     $download_page = Invoke-WebRequest -Uri "https://www.splunk.com/en_us/download/universal-forwarder.html" -UseBasicParsing -UserAgent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36"
 
-    $x86matched = $download_page.Content -match 'data-filename="(splunkforwarder-\S+-x86-release\.msi)"'
+    $x86matched = $download_page.Content -match 'data-filename="(splunkforwarder-\S+-x86-release\.msi)" data-link="(https://\S+\.msi)"'
     if ($x86matched) {
         $x86_filename = $matches[1]
+        $x86_url = $matches[2]
     }
     Else
     {
         Write-Error "Could not find x86 file"
     }
 
-    $x64matched = $download_page.Content -match 'data-filename="(splunkforwarder-\S+-x64-release\.msi)"'
+    $x64matched = $download_page.Content -match 'data-filename="(splunkforwarder-\S+-x64-release\.msi)" data-link="(https://\S+\.msi)"'
     if ($x64matched) {
         $x64_filename = $matches[1]
+        $x64_url = $matches[2]
     }
     Else
     {
@@ -45,9 +47,6 @@ function global:au_GetLatest {
     }
 
     $version = ($x64_filename -split '-')[1]
-
-    $x86_url = "https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86&platform=windows&version=$version&product=universalforwarder&filename=$x86_filename&wget=true"
-    $x64_url = "https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=windows&version=$version&product=universalforwarder&filename=$x64_filename&wget=true"
 
     $release_notes = "https://docs.splunk.com/Documentation/Forwarder/$version/Forwarder/KnownIssues"
 
