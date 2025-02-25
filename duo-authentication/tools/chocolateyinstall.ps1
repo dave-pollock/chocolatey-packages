4d9658587e346f2ba3043eb18e8e2b8971e2ab52
ChocolateyAutoUpdate <chocolatey@github.com>
2025-02-25 00:03:39 +0000

    AU: 1 updated - duo-authentication [skip ci] https://gist.github.com/dave-pollock/4d4c22e3d439bfee8dd5d04bed88a371/8cd9dd9e58bbd5c4b4567d14f7f76a91e06999a2

--- duo-authentication/tools/chocolateyinstall.ps1
+++ duo-authentication/tools/chocolateyinstall.ps1
@@ -15,8 +15,8 @@ $packageArgs = @{
   softwareName  = 'Duo Authentication for Windows Logon*'
   unzipLocation = $toolsDir
   fileType      = 'exe'
-  url           = 'https://dl.duosecurity.com/duo-win-login-4.3.1.exe'
-  checksum      = '504cce9f9b2cb7a0de1fc48eb7d5af36b3440f5fec3ab17ec8e5f43896222cf1'
+  url           = 'https://dl.duosecurity.com/duo-win-login-5.0.0.exe'
+  checksum      = '6609ed85dd2a9112e4fb5802751ea8915e17a1a93410422986a0892a914824cc'
   checksumType  = 'sha256'
   silentArgs    = $silentArgs
 }
